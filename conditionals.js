9f8326e1ad611ce8b393f718db5136af4e488e6b
Morsal Niyaz <morsalniyaz@gmail.com>
2021-10-05 10:32:09 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,17 +1,19 @@
-// const raining = true;
-// const cold = true;
+//EXAMPLE 1
+const raining = true;
+const cold = true;
 
-// if (raining) {
-//   console.log('Don\'t forget your umbrella!');
-// }
+if (raining) {
+  console.log('Don\'t forget your umbrella!');
+}
 
-// if (cold) {
-//   console.log('Makke sure to wear a scarf!');
-// }
+if (cold) {
+  console.log('Makke sure to wear a scarf!');
+}
 
-// console.log('Now you\'re ready to go outside!');
+console.log('Now you\'re ready to go outside!');
 
-//PART 2
+/************************************************** */
+//EXAMPLE 2
 const temperature = 12;
 
 if (temperature<0) {
@@ -23,3 +25,30 @@ if (temperature<0) {
 }
 
 console.log('Now you\'re ready to go outside!');
+
+/************************************************** */
+//EXAMPLE 3
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age >18) {
+  console.log('You are eligible to vote!');
+}
+//isCitizen and age both need to be true for the if statement to run
+
+/************************************************** */
+//EXAMPLE 4
+const temperatureCelsius = 45;
+
+if (temperatureCelsius<-40 || temperatureCelsius >40) {
+  console.log('Maybe it\' not a good idea to go outside today!');
+}
+
+/************************************************** */
+//EXAMPLE 5
+
+const snow = false;
+
+if (!snow) {
+console.log('It is not snowing, maybe tomorrow!');
+}
