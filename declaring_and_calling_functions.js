d6df0e1c243ee628569f745a2e63f8f4312ec665
Morsal Niyaz <morsalniyaz@gmail.com>
2021-10-06 15:15:13 -0700

    practice

---
+++ declaring_and_calling_functions.js
@@ -0,0 +1,14 @@
+
+//HELLO.js example
+const sayHello = function (name) {
+  console.log('Hello, ' + name);
+}
+
+//if we do a return then it won't run anything else below
+sayHello('Morsal');
+sayHello('Alim');
+sayHello('tba');
+//console.log(sayHello());
+
+
+//EVEN.js example
